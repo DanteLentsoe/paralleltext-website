aaa22a371553070d43fd150b9aebf47917179c17
Dante Lentsoe <dllentsoe@gmail.com>
2024-10-27 09:24:33 +0200

    feat: add `GithubSVGIcon` svg component

---
+++ src/components/SVG/ChromeSVGIcon/ChromeSVGIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const ChromeSVGIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-chrome"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <circle cx="12" cy="12" r="4" />
+      <line x1="21.17" x2="12" y1="8" y2="8" />
+      <line x1="3.95" x2="8.54" y1="6.06" y2="14" />
+      <line x1="10.88" x2="15.46" y1="21.94" y2="14" />
+    </svg>
+  )
+}
